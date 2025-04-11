fb9a154443dfb3845af13f6d569210a3234e7ab8
Elmer Izarra <eizarrac@uni.pe>
2025-04-10 20:54:08 -0500

    Implementamos la clase Question

---
+++ src/trivia.py
@@ -0,0 +1,11 @@
+
+class Question:
+    def __init__(self, question, options, correct_answer):
+        self.question = question
+        self.options = options
+        self.correct_answer = correct_answer
+
+    def is_correct(self, answer):
+        return self.correct_answer == answer
+
+  
