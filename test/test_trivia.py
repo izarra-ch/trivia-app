760637480ed6589ff618671bb95158d1de255d69
Elmer Izarra <eizarrac@uni.pe>
2025-04-13 23:25:13 -0500

    agregando pruebas unitarias para Quiz

--- test/test_trivia.py
+++ test/test_trivia.py
@@ -1,6 +1,6 @@
 import pytest
 
-from trivia import Question
+from trivia import Question, Quiz
 
 def test_question_correct_answer():
     question = Question("What is 2 + 2?", ["1", "2", "3", "4"], "4")
@@ -10,3 +10,13 @@ def test_question_correct_answer():
 def test_question_incorrect_answer():
     question = Question("What is 2 + 2?", ["1", "2", "3", "4"], "4")
     assert not question.is_correct("2")
+
+def test_quiz_scoring():
+    quiz = Quiz()
+    question = Question("What is 2 + 2?", ["1", "2", "3", "4"], "4")
+    quiz.add_question(question)
+    assert quiz.answer_question(question, "4") == True
+    assert quiz.correct_answers == 1
+    
+    assert quiz.answer_question(question, "1") == False
+    assert quiz.incorrect_answers == 1
