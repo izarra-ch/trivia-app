7ece5360acc434dd8fc7af797ea846068b745adb
Elmer Izarra <eizarrac@uni.pe>
2025-04-13 23:15:05 -0500

    Implementación de la clase Quiz y flujo del juego

--- src/trivia.py
+++ src/trivia.py
@@ -29,4 +29,67 @@ class Question:
         """
         return self.correct_answer == answer
 
-  
+class Quiz:
+
+    def __init__(self):
+        self.questions = []
+        self.current_question = 0
+        self.correct_answers = 0
+        self.incorrect_answers = 0
+
+    def add_question(self, question: Question) -> None:
+        self.questions.append(question)
+        
+    def get_next_question(self) -> Question:
+        if self.current_question < len(self.questions):
+            question = self.questions[self.current_question]
+            self.current_question += 1
+            return question
+        return None
+    
+    def answer_question(self, question: Question, answer: str) -> bool:
+      
+        if question.is_correct(answer):
+            self.correct_answers += 1
+            return True
+        else:
+            self.incorrect_answers += 1
+            return False
+          
+    def show_question_results(self) -> None:
+        print(f"Preguntas contestadas: {self.current_question}")
+        print(f"Respuestas correctas: {self.correct_answers}")
+        print(f"Respuestas incorrectas: {self.incorrect_answers}")
+
+    def start_quiz(self):
+        print("Bienvenido al juego de trivia!")
+        print("Responde las siguientes preguntas seleccionando el número de la opción correcta.")
+                
+        while self.current_question < len(self.questions):
+            question = self.get_next_question()
+            if question:
+                print(f"Pregunta {self.current_question}: {question.question}")
+                for idx, option in enumerate(question.options):
+                    print(f"{idx + 1}) {option}")
+                
+                while True:
+                    try:
+                        answer_to_question = int(input("Tu respuesta: "))
+                        if answer_to_question in range(1, 5):
+                            break
+                        else:
+                            print("Ingrese el numero de opción correcta.")
+                    except ValueError:
+                      print("Entrada inválida. Ingrese el numero de las opciones.")
+                answer = question.options[answer_to_question-1]
+                      
+                if self.answer_question(question, answer):
+                    print("¡Correcto!")
+                else:
+                    print("Incorrecto.")
+            else:
+                break
+
+        print("Juego terminado.")
+        self.show_question_results()
+        
