586ea982c43a57d20a9f78fbd76862c5c08e859c
Elmer Izarra <eizarrac@uni.pe>
2025-04-13 23:18:02 -0500

    Modificando main.py para iniciar el juego de trivia

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
-import uvicorn
-from fastapi import FastAPI
+from trivia import Question, Quiz
 
-app = FastAPI()
-
-@app.get('/')
-def home():
-  return {"server": "Servidor iniciado"}
+if __name__ == "__main__":
+    question_bank = [
+        Question("¿Cuál es la capital de Francia?", ["Madrid", "Londres", "París", "Berlín"], "París"),
+        Question("¿En qué país se encuentra la Torre Eiffel?", ["Italia", "Francia", "España", "Alemania"], "Francia"),
+        Question("¿Quién escribió 'Cien años de soledad'?", ["Mario Vargas Llosa", "Gabriel García Márquez", "Julio Cortázar", "Pablo Neruda"], "Gabriel García Márquez"),
+        Question("¿Cuál es el resultado de 7 x 8?", ["54", "56", "64", "58"], "56"),
+        Question("¿Cuál es el océano más grande del mundo?", ["Atlántico", "Índico", "Ártico", "Pacífico"], "Pacífico"),
+        Question("¿Cuál es el idioma más hablado en el mundo?", ["Español", "Inglés", "Chino mandarín", "Hindi"], "Inglés"),
+        Question("¿Qué parte del cuerpo bombea la sangre?", ["Pulmones", "Riñón", "Hígado", "Corazón"], 4),
+    ]
+    
+    quiz = Quiz()
+    
+    for question in question_bank:
+        quiz.add_question(question)
+    
+    quiz.start_quiz()
